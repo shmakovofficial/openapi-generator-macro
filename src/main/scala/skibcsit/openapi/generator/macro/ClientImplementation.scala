c702954f8c5dcc8e396ff5395e5c76319b70edeb
Aleksandr Shmakov <probablyviolin@gmail.com>
2020-12-16 17:56:45 +0300

    Code improvements

--- src/main/scala/skibcsit/openapi/generator/macro/ClientImplementation.scala
+++ src/main/scala/skibcsit/openapi/generator/macro/ClientImplementation.scala
@@ -18,9 +18,13 @@ object ClientImplementation extends StaticAnnotation {
     val annotationString: String = String.valueOf(extractAnnotationIdent(c)(c.prefix.tree.children).name)
     val serviceName: TypeName = traitDef.name
     val openAPI: OpenAPI = new OpenAPIParser().readLocation(pathString, null, null).getOpenAPI
-    val methodsImpl: List[DefDef] = extractDefs(c)(traitDef).map(mapDef(c)(_))
+    val endpoints: List[DefDef] = extractDefs(c)(traitDef).map(toEndpoint(openAPI)(c)(_))
+    val endpointsImpl: List[DefDef] = endpoints.map((defDef: DefDef) => annotationString match {
+      case "Http4sImplementation" => http4sImplementation(openAPI)(c)(defDef)
+      case _ => sttpImplementation(openAPI)(c)(defDef)
+    })
     val emptyBlock: Block = q"new $serviceName {}".asInstanceOf[Block]
-    val fullBlock: Block = fillBlock(c)(emptyBlock, methodsImpl)
+    val fullBlock: Block = fillBlock(c)(emptyBlock, endpointsImpl)
     val implVal: ValDef = q"implicit val impl: $serviceName = $fullBlock".asInstanceOf[ValDef]
     val emptyModule: ModuleDef = q"object ${serviceName.toTermName} {}".asInstanceOf[ModuleDef]
     val fullModule: ModuleDef = fillModule(c)(emptyModule, implVal)
@@ -29,13 +33,41 @@ object ClientImplementation extends StaticAnnotation {
     c.Expr[Any](resultBlock)
   }
 
-  def fillModule(c: whitebox.Context)(oldModuleDef: c.universe.ModuleDef, valDef: c.universe.ValDef): c.universe.ModuleDef = {
-    val oldTemplate: c.universe.Template = oldModuleDef.children.head.asInstanceOf[c.universe.Template]
-    val newTemplate: c.universe.Template = c.universe.Template(oldTemplate.parents, oldTemplate.self, List(oldTemplate.body.head, valDef))
-    c.universe.ModuleDef(oldModuleDef.mods, oldModuleDef.name, newTemplate)
+  private def extractTraitDef(c: whitebox.Context)(annottees: Seq[c.Expr[Any]]): c.universe.ClassDef =
+    annottees.head.asInstanceOf[c.Expr[Nothing]].tree.asInstanceOf[c.universe.ClassDef]
+
+  private def extractPath(c: whitebox.Context)(annotationList: List[c.Tree]): String =
+    String.valueOf(extractPathLiteral(c)(annotationList).value.value)
+
+  private def extractPathLiteral(c: whitebox.Context)(annotationList: List[c.Tree]): c.universe.Literal =
+    annotationList.tail.head match {
+      case namedArg: c.universe.NamedArg => namedArg.rhs.asInstanceOf[c.universe.Literal]
+      case tree: c.Tree => tree.asInstanceOf[c.universe.Literal]
+    }
+
+  private def extractAnnotationIdent(c: whitebox.Context)(annotationList: List[c.Tree]): c.universe.Ident =
+    annotationList.head.asInstanceOf[c.universe.Select]
+      .qualifier.asInstanceOf[c.universe.New]
+      .tpt.asInstanceOf[c.universe.Ident]
+
+  private def extractDefs(c: whitebox.Context)(traitDef: c.universe.ClassDef): List[c.universe.DefDef] =
+    traitDef.children.head.asInstanceOf[c.universe.Template].body.map(_.asInstanceOf[c.universe.DefDef])
+
+  private def toEndpoint(openAPI: OpenAPI)(c: whitebox.Context)(defDef: c.universe.DefDef): c.universe.DefDef =
+    c.universe.DefDef(c.universe.Modifiers(c.universe.Flag.OVERRIDE), defDef.name, defDef.tparams, defDef.vparamss, defDef.tpt, generateRhs(c)(openAPI)(defDef.name.toString))
+
+  private def generateRhs(c: whitebox.Context)(openAPI: OpenAPI)(name: String): c.Tree = throwNotImplemented(c)
+
+  private def throwNotImplemented(c: whitebox.Context): c.Tree = {
+    import c.universe.Quasiquote
+    q"???"
   }
 
-  def fillBlock(c: whitebox.Context)(block: c.universe.Block, defs: List[c.universe.Tree]): c.universe.Block = {
+  private def http4sImplementation(openAPI: OpenAPI)(c: whitebox.Context)(defDef: c.universe.DefDef): c.universe.DefDef = defDef
+
+  private def sttpImplementation(openAPI: OpenAPI)(c: whitebox.Context)(defDef: c.universe.DefDef): c.universe.DefDef = defDef
+
+  private def fillBlock(c: whitebox.Context)(block: c.universe.Block, defs: List[c.universe.Tree]): c.universe.Block = {
     val oldClassDef: c.universe.ClassDef = block.children.head.asInstanceOf[c.universe.ClassDef]
     val oldTemplate: c.universe.Template = oldClassDef.children.head.asInstanceOf[c.universe.Template]
     val newTemplate: c.universe.Template = c.universe.Template(oldTemplate.parents, oldTemplate.self, oldTemplate.children.last :: defs)
@@ -43,39 +75,10 @@ object ClientImplementation extends StaticAnnotation {
     c.universe.Block(List(newClassDef), block.children.last)
   }
 
-  def throwNotImplemented(c: whitebox.Context): c.Tree = {
-    import c.universe.Quasiquote
-    q"???"
+  private def fillModule(c: whitebox.Context)(oldModuleDef: c.universe.ModuleDef, valDef: c.universe.ValDef): c.universe.ModuleDef = {
+    val oldTemplate: c.universe.Template = oldModuleDef.children.head.asInstanceOf[c.universe.Template]
+    val newTemplate: c.universe.Template = c.universe.Template(oldTemplate.parents, oldTemplate.self, List(oldTemplate.body.head, valDef))
+    c.universe.ModuleDef(oldModuleDef.mods, oldModuleDef.name, newTemplate)
   }
 
-  def mapDef(c: whitebox.Context)(defDef: c.universe.DefDef): c.universe.DefDef =
-    c.universe.DefDef(c.universe.Modifiers(c.universe.Flag.OVERRIDE), defDef.name, defDef.tparams, defDef.vparamss, defDef.tpt, throwNotImplemented(c))
-
-  def addFlags(c: whitebox.Context)(flagSets: c.universe.FlagSet*): c.universe.FlagSet = {
-    import c.universe._
-    flagSets.reduce((set1: c.universe.FlagSet, set2: c.universe.FlagSet) => set1 | set2)
-  }
-
-  def extractDefs(c: whitebox.Context)(traitDef: c.universe.ClassDef): List[c.universe.DefDef] =
-    traitDef.children.head.asInstanceOf[c.universe.Template].body.map(_.asInstanceOf[c.universe.DefDef])
-
-  def extractParents(c: whitebox.Context)(traitDef: c.universe.ClassDef): List[c.Tree] =
-    traitDef.children.head.asInstanceOf[c.universe.Template].parents
-
-  def extractPath(c: whitebox.Context)(annotationList: List[c.Tree]): String =
-    String.valueOf(extractPathLiteral(c)(annotationList).value.value)
-
-  def extractPathLiteral(c: whitebox.Context)(annotationList: List[c.Tree]): c.universe.Literal =
-    annotationList.tail.head match {
-      case namedArg: c.universe.NamedArg => namedArg.rhs.asInstanceOf[c.universe.Literal]
-      case tree: c.Tree => tree.asInstanceOf[c.universe.Literal]
-    }
-
-  def extractAnnotationIdent(c: whitebox.Context)(annotationList: List[c.Tree]): c.universe.Ident =
-    annotationList.head.asInstanceOf[c.universe.Select]
-      .qualifier.asInstanceOf[c.universe.New]
-      .tpt.asInstanceOf[c.universe.Ident]
-
-  def extractTraitDef(c: whitebox.Context)(annottees: Seq[c.Expr[Any]]): c.universe.ClassDef =
-    annottees.head.asInstanceOf[c.Expr[Nothing]].tree.asInstanceOf[c.universe.ClassDef]
 }
